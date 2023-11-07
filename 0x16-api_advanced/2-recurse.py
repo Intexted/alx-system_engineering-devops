9f6d48477434bd19e50b23470fefa18a7796752d
Intexted <lotfiaouane@gmail.com>
2023-11-07 04:04:03 -0800

    0x16-api_advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,6 +6,7 @@ the top ten hot posts of a subreddit
 import requests
 import sys
 
+
 def add_title(hot_list, hot_posts):
     """ Adds item into a list """
     if len(hot_posts) == 0:
@@ -14,11 +15,12 @@ def add_title(hot_list, hot_posts):
     hot_posts.pop(0)
     add_title(hot_list, hot_posts)
 
+
 def recurse(subreddit, hot_list=[], after=None):
-    """ Queries the Reddit API and prints top ten hot posts """
-    user_agent = 'Mozilla/5.0'
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
     headers = {
-        'User-Agent': user_agent
+        'User-Agent': u_agent
     }
 
     params = {
@@ -26,19 +28,18 @@ def recurse(subreddit, hot_list=[], after=None):
     }
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(url,
-                            headers=headers,
-                            params=params,
-                            allow_redirects=False)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
 
-    if response.status_code != 200:
+    if res.status_code != 200:
         return None
 
-    data = response.json()
-    hot_posts = data['data']['children']
+    dic = res.json()
+    hot_posts = dic['data']['children']
     add_title(hot_list, hot_posts)
-    after = data['data']['after']
+    after = dic['data']['after']
     if not after:
         return hot_list
     return recurse(subreddit, hot_list=hot_list, after=after)
-
